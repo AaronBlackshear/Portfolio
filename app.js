424843a146f6d4bba9c8e4b9712b061a3a88f626
Aaron Blackshear <theshiftybapple@gmail.com>
2018-05-12 19:35:02 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,21 @@
+function scrollToAbout() {
+    window.scrollTo({ top: 550, behavior: "smooth" })
+}
+
+function scrollToSkills() {
+    window.scrollTo({ top: 950, behavior: "smooth" })
+}
+function scrollToProjects() {
+    window.scrollTo({ top: 1350, behavior: "smooth" })
+}
+function scrollToContact() {
+    window.scrollTo({ top: 1750, behavior: "smooth" })
+}
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" })
+}
+
+function openConnectDev() {
+    window.open('https://connectdevs.com')
+}
