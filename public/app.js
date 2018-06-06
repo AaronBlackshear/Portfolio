b36261557e036b1ead5e5434fee1d10b5845a715
Aaron Blackshear <theshiftybapple@gmail.com>
2018-06-06 09:25:35 -0500

    Portfolio Moblie Responsive

---
+++ public/app.js
@@ -0,0 +1,25 @@
+function scrollToAbout() {
+  window.scrollTo({ top: 620, behavior: "smooth" });
+}
+
+function scrollToSkills() {
+  window.scrollTo({ top: 1000, behavior: "smooth" });
+}
+function scrollToProjects() {
+  window.scrollTo({ top: 1550, behavior: "smooth" });
+}
+function scrollToContact() {
+  window.scrollTo({ top: 10000, behavior: "smooth" });
+}
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+function openConnectDev() {
+  window.open("https://connectdevs.com");
+}
+
+function openWishTrip() {
+  window.open("http://wishtrip.online");
+}
